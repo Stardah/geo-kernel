4edaf70693a49de8782a9953f9b886f47873d7dc
Никита Терлыч <naterlych@edu.hse.ru>
2019-04-22 08:07:50 +0300

    Автодополнение, инспекции, комментарии.

--- geokernel.py
+++ geokernel.py
@@ -16,9 +16,48 @@ password = ""
 token = ""
 port = 9090
 
-class Connection():
+class GeoKernel(Kernel):
+    # Kernel info
+    implementation = 'GEO'
+    implementation_version = '1.0'
+    language = 'geo'
+    language_version = '1.0'
+    language_info = {'name': 'geo',
+                     'mimetype': 'text/x-geo',  # mimetype for script files in this language
+                     'codemirror_mode': 'geo',
+                     'extension': '.geo'}
+    banner = "GEO Console"
 
-    def request_ws(code):
+    # commands and its arguments for map object
+    mapCommands = {"create":["-map", "-x", "-y", "-zoom"], 
+    "show":["-map"], 
+    "delete":["-map"], 
+    "update":["-map", "-name", "-x", "-y", "-zoom"], 
+    "login":[]
+    }
+    # commands and its arguments for layer object
+    layerCommands = {"create":["-map","-layer", "-link"], 
+    "show":["-layer"], 
+    "delete":["-layer"], 
+    "update":["-layer", "-name", "-link"], 
+    "login":[]
+    }
+    # list of CRUD commands for both map and layer objects
+    commonComands = ["create", "show", "delete", "update"]
+    words = ["layer", "map"]
+    # dict of commands descriptions
+    inspections = {
+        "map show" : "Returns an html document containing the map with all its layers\n  [-map]:string",
+        "map create" : "Creates new map with name [-map]\n [-map]:string [-x]:double [-y]:double [-zoom]:int",
+        "map update" : "Changes map attributes\n Required: [-map]:string\n Optional: [-name]:string [-x]:double [-y]:double [-zoom]:int",
+        "map delete" : "Deletes map\n [-map]:string",
+        "layer show" : "Returns a string of layer attributes",
+        "layer create" : "Adds new layer [-layer] to a map [-map]\n [-layer]:string [-map]:string",
+        "layer update" : "Changes layer attributes\n Required: [-layer]:string\n Optional: [-name]:string [-link]:string",
+        "layer delete" : "Removes layer\n [-layer]:string"
+    }
+
+    def request_ws(self, code):
         ''' Send code execution request to a server
                 Args:
                     code: commands to be executed.
@@ -36,50 +75,27 @@ class Connection():
                 token = result
             return result
         except Exception as e:
-            open('log.txt', 'a').write(str(e) + "\n")
             return "Error"
 
-async def client(code):
-    async with websockets.connect('ws://localhost:9090') as websocket:
-        await websocket.send(code)
-        response = await websocket.recv()
-        return response
-
-class GeoKernel(Kernel):
-    implementation = 'GEO'
-    implementation_version = '1.0'
-    language = 'geo'
-    language_version = '1.0'
-    language_info = {'name': 'geo',
-                     'mimetype': 'text/x-geo',  # mimetype for script files in this language
-                     'codemirror_mode': 'geo',
-                     'extension': '.geo'}
-    banner = "GEO Console"
-
-    commands = ["create", "show", "delete", "update", "login"]
-    subCommands = ["layer", "map"]
-    inspections = {
-        "create" : "creates new object",
-        "update" : "updates new object",
-        "show map" : "returns an html document containing the map with all it's layers\n map_name:string",
-        "create map" : "name:string x:double y:double zoom:int",
-        "update map" : "name:string new_name:string",
-        "create layer" : "name:string layer:string",
-    }
-
     def do_log(self, text):
+        ''' Writes text to log file
+                Args: text: text to be writen.
+        '''
         open('log.txt','a').write(text+"\n")
 
     def do_complete(self, code, cursor_pos):
-        self.do_log('do_complete \n') #str(code) + str(cursor_pos)
         matches = []
-        last_command = code[:cursor_pos].split("\n")[-1].strip().split(" ")[-1]
-        if last_command in self.commands:
-            matches = self.subCommands
-        elif last_command in self.subCommands:
-            matches = self.commands
+        last_line = code[:cursor_pos].split("\n")[-1].strip()
+        if len(last_line.split()) > 1:
+            obj, command = last_line.split()[:2]
+            if obj == "layer" and command in self.layerCommands:
+                matches = self.layerCommands[command]
+            elif obj == "map" and command in self.mapCommands:
+                matches = self.mapCommands[command]
+        elif len(last_line.split()) == 1:
+            matches = self.commonComands
         if code[cursor_pos-1] != " ":
-            matches = [" "+i for i in matches]
+                matches = [" "+i for i in matches]    
         return {
             'status' : 'ok',
             'cursor_start' : cursor_pos,
@@ -87,7 +103,6 @@ class GeoKernel(Kernel):
             'matches' : matches}
 
     def do_inspect(self, code, cursor_pos, detail_level=0):
-        self.do_log("do_inspect")
         commands = code[:cursor_pos].split("\n")[-1].strip()
         if len(commands.split()) > 2:
             commands = " ".join(commands.split()[:2])
@@ -103,30 +118,44 @@ class GeoKernel(Kernel):
         return content
 
     def login_request(self):
+        ''' Requests token and generates login form
+            Returns: html login form
+        '''
         global port
-        token = Connection.request_ws("token")
+        # request token
+        token = self.request_ws("token")
         if token != 'Error':
-            data = """<iframe width="300" height="300" srcdoc="<html><style>/* Full-width inputs */ input[type=text], input[type=password]{width: 100%; padding: 12px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; box-sizing: border-box;}/* Set a style for all buttons */ button{background-color: #4e91fc; color: white; padding: 14px 20px; margin: 8px 0; border: none; cursor: pointer; width: 100%;}/* Add a hover effect for buttons */ button:hover{opacity: 0.8;}</style> <div class='container'> <label for='uname'><b>Username</b></label> <input type='text' id='loginInput' placeholder='Enter Username' name='uname' required> <label for='psw'><b>Password</b></label> <input type='password' id='passwordInput' placeholder='Enter Password' name='psw' required> </div><button class='check'>Login</button> <div class='state'> Result: <span class='value'>?</span> </div><script type='text/javascript'> var url='ws://127.0.0.1:9090/'; var check=document.querySelector('.check'), value=document.querySelector('.value'), websocket=new WebSocket(url); check.onclick=function (event){var login=document.getElementById('loginInput').value; var password=document.getElementById('passwordInput').value; var token='"""+token+"""'; websocket.send('login '+login+' '+password+' '+token);}; websocket.onmessage=function (event){value.textContent=event.data;}; </script> </html>" frameborder="0" allowfullscreen=""></iframe>"""
+            data = """<iframe width="300" height="250" srcdoc="<html><style>/* Full-width inputs */ input[type=text], input[type=password]{width: 100%; padding: 12px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; box-sizing: border-box;}/* Set a style for all buttons */ button{background-color: #4e91fc; color: white; padding: 14px 20px; margin: 8px 0; border: none; cursor: pointer; width: 100%;}/* Add a hover effect for buttons */ button:hover{opacity: 0.8;}</style> <div class='container'> <label for='uname'><b>Username</b></label> <input type='text' id='loginInput' placeholder='Enter Username' name='uname' required> <label for='psw'><b>Password</b></label> <input type='password' id='passwordInput' placeholder='Enter Password' name='psw' required> </div><button class='check'>Login</button> <div class='state'> Result: <span class='value'>?</span> </div><script type='text/javascript'> var url='ws://127.0.0.1:9090/'; var check=document.querySelector('.check'), value=document.querySelector('.value'), websocket=new WebSocket(url); check.onclick=function (event){var login=document.getElementById('loginInput').value; var password=document.getElementById('passwordInput').value; var token='"""+token+"""'; websocket.send('login '+login+' '+password+' '+token);}; websocket.onmessage=function (event){value.textContent=event.data;}; </script> </html>" frameborder="0" allowfullscreen=""></iframe>"""
             self.send_html(data)
         else:
             self.send_html(token)
 
     def register_request(self):
         global port
-        data = """<iframe width="300" height="300" srcdoc="<html><style>/* Full-width inputs */ input[type=text], input[type=password]{width: 100%; padding: 12px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; box-sizing: border-box;}/* Set a style for all buttons */ button{background-color: #4e91fc; color: white; padding: 14px 20px; margin: 8px 0; border: none; cursor: pointer; width: 100%;}/* Add a hover effect for buttons */ button:hover{opacity: 0.8;}</style> <div class='container'> <label for='uname'><b>Username</b></label> <input type='text' id='loginInput' placeholder='Enter Username' name='uname' required> <label for='psw'><b>Password</b></label> <input type='password' id='passwordInput' placeholder='Enter Password' name='psw' required> </div><button class='check'>Login</button> <div class='state'> Result: <span class='value'>?</span> </div><script type='text/javascript'> var url='ws://127.0.0.1:9090/'; var check=document.querySelector('.check'), value=document.querySelector('.value'), websocket=new WebSocket(url); check.onclick=function (event){var login=document.getElementById('loginInput').value; var password=document.getElementById('passwordInput').value; websocket.send(login+' '+password);}; websocket.onmessage=function (event){value.textContent=event.data;}; </script> </html>" frameborder="0" allowfullscreen=""></iframe>"""
+        data = """<iframe width="300" height="250" srcdoc="<html><style>/* Full-width inputs */ input[type=text], input[type=password]{width: 100%; padding: 12px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; box-sizing: border-box;}/* Set a style for all buttons */ button{background-color: #4e91fc; color: white; padding: 14px 20px; margin: 8px 0; border: none; cursor: pointer; width: 100%;}/* Add a hover effect for buttons */ button:hover{opacity: 0.8;}</style> <div class='container'> <label for='uname'><b>Username</b></label> <input type='text' id='loginInput' placeholder='Enter Username' name='uname' required> <label for='psw'><b>Password</b></label> <input type='password' id='passwordInput' placeholder='Enter Password' name='psw' required> </div><button class='check'>Login</button> <div class='state'> Result: <span class='value'>?</span> </div><script type='text/javascript'> var url='ws://127.0.0.1:9090/'; var check=document.querySelector('.check'), value=document.querySelector('.value'), websocket=new WebSocket(url); check.onclick=function (event){var login=document.getElementById('loginInput').value; var password=document.getElementById('passwordInput').value; websocket.send(login+' '+password);}; websocket.onmessage=function (event){value.textContent=event.data;}; </script> </html>" frameborder="0" allowfullscreen=""></iframe>"""
         self.send_html(data)
 
     def send_html(self, data):
-        #display_data
+        ''' Send message with html data to the frontend via iopub socket
+                Args:
+                    data: any data to be sent as an html.
+        '''
+        # display_data message
         self.send_response(self.iopub_socket,
-            'execute_result',
+            'display_data',
             {
-                'execution_count': self.execution_count,
                 'data': {'text/html' : data},
                 'metadata':{}
             })
 
     def prepare_code(self, code):
+        ''' Adds token to a code string
+                Args:
+                    code: commands to be executed.
+
+                Returns:
+                    String with token to be sent to a server
+        '''
         global token
         return "-".join([token, code])
 
@@ -137,25 +166,12 @@ class GeoKernel(Kernel):
 
         if 'login' in code:
             self.login_request()
-        #elif 'register' in code:
-        #    self.register_request()
+        elif 'register' in code:
+            pass
         elif not silent:
-            #rcode = self.prepare_code(code)
-            server_response = Connection.request_ws(self.prepare_code(code))
-            if "show" in code:
-                # Send data to be displayed in a cell
-                self.send_html(server_response)
-            else:
-                # Send the output to the client
-                self.send_response(
-                    self.iopub_socket,
-                    'stream', {
-                        'name': 'stdout',
-                        'text':server_response,
-                        'data': {
-                                #'text/html' : server_response
-                            }
-                        })
+            server_response = self.request_ws(self.prepare_code(code))
+            # Send data to be displayed in a cell
+            self.send_html(server_response)
         # Return the exection results
         return {'status': 'ok',
                 'execution_count': self.execution_count,
